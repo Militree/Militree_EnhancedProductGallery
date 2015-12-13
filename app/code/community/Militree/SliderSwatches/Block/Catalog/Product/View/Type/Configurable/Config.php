f6b0c47f5892db48b522413625d506747d7935f1
Brandyn Bold <brandynbold@gmail.com>
2015-12-12 22:24:49 -0500

    Remove incriminating comments and prep for a release

--- app/code/community/Militree/SliderSwatches/Block/Catalog/Product/View/Type/Configurable/Config.php
+++ app/code/community/Militree/SliderSwatches/Block/Catalog/Product/View/Type/Configurable/Config.php
@@ -19,8 +19,6 @@ class Militree_SliderSwatches_Block_Catalog_Product_View_Type_Configurable_Confi
                 'images' => []
             ]);
 
-            // I am aware of what is happening here. Fix this later.
-            // For real, don't let this get committed.
             $product = Mage::getModel('catalog/product')->load($id);
             $imagesIndex = 0;
             $images = $product->getMediaGalleryImages();
